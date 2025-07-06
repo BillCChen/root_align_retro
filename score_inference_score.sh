8e08c0e3f60f5db014c2bb18ef16bbc3c0f03c79
jianxing.hu <jianxing.hu@xtalpi.com>
2025-07-06 09:00:59 +0800

    cotinue training

--- score_inference_score.sh
+++ score_inference_score.sh
@@ -1,13 +1,13 @@
-CUDA_VISIBLE_DEVICES=0,1 onmt_translate -config pretrain_finetune/finetune/PtoTMPtoR/TMPtoR-50K-aug20-translate.yml 
+onmt_translate -config pretrain_finetune/finetune/PtoTMPtoR/TMPtoR-50K-aug20-translate.yml 
 
 python score.py \
 	-beam_size 10 \
 	-n_best 10 \
 	-augmentation 20 \
-	-targets /root/reaction_data/pretrain_aug/USPTO_50K_PtoTMPtoR_aug20/val/tmp_reactant-mini.txt \
+	-targets /root/reaction_data/pretrain_aug/USPTO_50K_PtoTMPtoR/test/tmp_reactant.txt \
 	-process_number 8 \
 	-score_alpha 1 \
-	-predictions sample/USPTO_50K_TMPtoR_aug20_trn_val_tst/finetune_TMP_model_26-30_TMP_product_val.txt \
-	-save_file   sample/USPTO_50K_TMPtoR_aug20_trn_val_tst/finetune_TMP_model_26-30_TMP_product_val_result.txt \
+	-predictions sample/USPTO_50K_TMPtoR_fromUSPTO50KP2R/finetune_average_model_26-30.txt \
+	-save_file   sample/USPTO_50K_TMPtoR_fromUSPTO50KP2R/finetune_average_model_26-30_result.txt \
 	-synthon 
 
--- score_inference_score_BJMU.sh
+++ score_inference_score_BJMU.sh
@@ -4,9 +4,9 @@ python score.py \
 	-beam_size 10 \
 	-n_best 10 \
 	-augmentation 20 \
-	-targets /root/reaction_data/pretrain_aug/USPTO_50K_PtoTMPtoR_aug20/test/tmp_reactant.txt \
-	-predictions sample/USPTO_50K_PtoTMP_aug20_fromUSPTO50KP2R/finetune_average_model_11-15_all.txt \
-	-save_file sample/USPTO_50K_PtoTMP_aug20_fromUSPTO50KP2R/finetune_average_model_11-15_all-results.txt \
+	-targets dataset/USPTO_50K_PtoTMPtoR/test/tmp_reactant.txt \
+	-predictions sample/USPTO_50K_TMPtoR_fromUSPTO50KP2R/finetune_average_model_11-15_all.txt \
+	-save_file sample/USPTO_50K_TMPtoR_fromUSPTO50KP2R/finetune_average_model_11-15_all-results.txt \
 	-process_number 8 \
 	-score_alpha 1 \
 	-synthon 
