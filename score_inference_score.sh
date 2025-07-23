b919a2c774a133f2b37193c187c0e6463cbf6f3f
jianxing.hu <jianxing.hu@xtalpi.com>
2025-07-23 21:14:47 +0800

    fast fix

--- score_inference_score.sh
+++ score_inference_score.sh
@@ -4,7 +4,7 @@
 # 50K
 # CUDA_VISIBLE_DEVICES=1 onmt_translate -config pretrain_finetune/finetune/PtoTMPtoR/TMPtoR-50K-aug20-translate.yml
 # FULL
-CUDA_VISIBLE_DEVICES=1 onmt_translate -config pretrain_finetune/finetune/PtoTMPtoR/TMPtoR-FULL-aug10-translate.yml
+CUDA_VISIBLE_DEVICES=1 onmt_translate -config pretrain_finetune/finetune/PtoTMPtoR/TMPtoR-MIT-translate.yml
 python score.py \
 	-beam_size 10 \
 	-n_best 10 \
@@ -12,7 +12,7 @@ python score.py \
 	-targets /root/reaction_data/pretrain_aug/USPTO_MIT_PtoTMPtoR_aug10/test/tmp_reactant.txt \
 	-process_number 8 \
 	-score_alpha 1 \
-	-predictions sample/USPTO_MIT_PtoTMPtoR_fromUSPTOMITP2R_aug10/finetune_model_14.txt \
-	-save_file   sample/USPTO_MIT_PtoTMPtoR_fromUSPTOMITP2R_aug10/finetune_model_14_result.txt \
+	-predictions sample/USPTO_MIT_PtoTMPtoR_fromUSPTOMITP2R_aug10/finetune_model_86-90.txt \
+	-save_file   sample/USPTO_MIT_PtoTMPtoR_fromUSPTOMITP2R_aug10/finetune_model_86-90_result.txt \
 	-synthon 
 
