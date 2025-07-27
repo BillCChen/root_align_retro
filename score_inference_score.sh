820531b45c413c91b00586f22d7248d836e9a32d
jianxing.hu <jianxing.hu@xtalpi.com>
2025-07-27 19:11:39 +0800

    fast fix

--- score_inference_score.sh
+++ score_inference_score.sh
@@ -6,7 +6,7 @@ python score.py \
 	-targets dataset/CHORISO_PtoTMPtoR_aug5/test/tmp_reactant_mini.txt \
 	-process_number 8 \
 	-score_alpha 1 \
-	-predictions sample/CHORISO_TMPtoR_fromUSPTOFULLP2R_aug10/finetune_model_26-30_CHORISO_mini.txt \
-	-save_file   sample/CHORISO_TMPtoR_fromUSPTOFULLP2R_aug10/finetune_model_26-30_CHORISO_mini_result.txt \
+	-predictions sample/CHORISO_TMPtoR_fromUSPTOFULLP2R_aug10/finetune_model_46-50_CHORISO_mini.txt \
+	-save_file   sample/CHORISO_TMPtoR_fromUSPTOFULLP2R_aug10/finetune_model_46-50_CHORISO_mini_result.txt \
 	-synthon 
 
