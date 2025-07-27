4e1717da8e0de7cccdccd988fefe68867ee7f56b
jianxing.hu <jianxing.hu@xtalpi.com>
2025-07-27 18:27:16 +0800

    fast fix

--- score_inference_score.sh
+++ score_inference_score.sh
@@ -1,4 +1,4 @@
-onmt_translate -config pretrain_finetune/finetune/PtoTMPtoR/TMPtoR-FULL-translate.yml
+# onmt_translate -config pretrain_finetune/finetune/PtoTMPtoR/TMPtoR-CHORISO-translate_BJMU.yml
 python score.py \
 	-beam_size 10 \
 	-n_best 10 \
@@ -6,7 +6,7 @@ python score.py \
 	-targets /root/reaction_data/pretrain_aug/CHORISO_PtoTMPtoR_aug5/test/tmp_reactant.txt \
 	-process_number 8 \
 	-score_alpha 1 \
-	-predictions sample/USPTO_FULL_TMPtoR_fromUSPTOFULLP2R_aug10/finetune_model_26-30_CHORISO_mini.txt \
-	-save_file   sample/USPTO_FULL_TMPtoR_fromUSPTOFULLP2R_aug10/finetune_model_26-30_CHORISO_mini_result.txt \
+	-predictions sample/CHORISO_TMPtoR_fromUSPTOFULLP2R_aug10/finetune_model_26-30_CHORISO_mini.txt \
+	-save_file   sample/CHORISO_TMPtoR_fromUSPTOFULLP2R_aug10/finetune_model_26-30_CHORISO_mini_result.txt \
 	-synthon 
 
